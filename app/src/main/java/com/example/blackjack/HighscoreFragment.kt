c91ec23ffb81879778ba59910e7e736c6c344f0a
DESKTOP-UIEJSCU\oTriks <maar_tiin@hotmail.com>
2023-11-23 02:39:00 +0100

    added elements into XLR game fragment

---
+++ app/src/main/java/com/example/blackjack/HighscoreFragment.kt
@@ -0,0 +1,28 @@
+package com.example.blackjack
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HighscoreFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view =
+        return inflater.inflate(R.layout.fragment_highscore, container, false)
+
+
+
+
+    }
+
+    }
