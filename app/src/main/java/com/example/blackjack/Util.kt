e420e7672b5f47a78fa5bbdd74622cd715968950
DESKTOP-UIEJSCU\oTriks <maar_tiin@hotmail.com>
2023-12-06 13:29:07 +0100

    DRY

--- app/src/main/java/com/example/blackjack/Util.kt
+++ app/src/main/java/com/example/blackjack/Util.kt
@@ -7,7 +7,6 @@ import androidx.annotation.ColorRes
 
 class Util {
 
-
     fun setTextViewBackground(textView: TextView, text: String, @ColorRes colorResId: Int) {
         textView.text = text
         val backgroundColor = textView.context.resources.getColor(colorResId, textView.context.theme) and 0x00FFFFFF or (128 shl 24)
@@ -24,23 +23,4 @@ class Util {
         textView.background = backgroundDrawable
         textView.gravity = Gravity.CENTER
     }
-
-//    fun TextView.setTextViewBackground(text: String, @ColorRes colorResId: Int) {
-//        this.text = text
-//        val backgroundColor = context.resources.getColor(colorResId, context.theme) and 0x00FFFFFF or (128 shl 24)
-//        val backgroundDrawable = GradientDrawable()
-//        backgroundDrawable.shape = GradientDrawable.RECTANGLE
-//        val cornerRadius = resources.getDimensionPixelSize(R.dimen.background_corner_radius_more_rounded).toFloat()
-//        backgroundDrawable.cornerRadius = cornerRadius
-//        val padding = resources.getDimensionPixelSize(R.dimen.background_padding)
-//        setPadding(padding, padding, padding, padding)
-//        val backgroundWidth = resources.getDimensionPixelSize(R.dimen.background_width)
-//        val backgroundHeight = resources.getDimensionPixelSize(R.dimen.background_height)
-//        backgroundDrawable.setSize(backgroundWidth, backgroundHeight)
-//        backgroundDrawable.setColor(backgroundColor)
-//        background = backgroundDrawable
-//        gravity = Gravity.CENTER
-//    }
-
-
 }
